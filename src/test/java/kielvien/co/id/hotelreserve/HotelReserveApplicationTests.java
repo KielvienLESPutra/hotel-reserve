a90fcc44c1df3e9b1420ee87a01ad4526cf8a14c
Kielvien L E S Putra <for2university@gmail.com>
2024-01-13 22:04:55 +0700

    Init commit

---
+++ src/test/java/kielvien/co/id/hotelreserve/HotelReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package kielvien.co.id.hotelreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
